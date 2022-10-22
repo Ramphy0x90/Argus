a93ed1a15ee76a7ed2361b4218bb6085666ec31e
ramphy0x90 <ramphy11@hotmail.com>
2022-10-22 19:44:03 +0200

    Test

--- zoho_log/app/views.py
+++ zoho_log/app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def index(request):
+    template = loader.get_template('index.html')
+
+    context = {
+        'dashboard_data': {
+            'Functions': {
+                'value': 3,
+                'icon': 'function.svg'
+            },
+            'Departments': {
+                'value': 5,
+                'icon': 'department.svg'
+            },
+            'Fails': {
+                'value': 2,
+                'icon': 'error.svg'
+            }
+        }
+    }
+
+    return HttpResponse(template.render(context, request))
