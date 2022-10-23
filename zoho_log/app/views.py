1b135bbc3433a462f5220231e74c45aaa02226f0
ramphy0x90 <ramphy11@hotmail.com>
2022-10-23 14:05:56 +0200

    Register function entry

--- zoho_log/app/views.py
+++ zoho_log/app/views.py
@@ -2,24 +2,86 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Function
+from .models import Ticket
+from .models import Department
+from .models import Log
+
+from ast import literal_eval
+
+
 def index(request):
     template = loader.get_template('index.html')
+
+    logs = Log.objects.all().values('function__name', 'ticket__number', 'log_in')
+    functions = Function.objects.all().values('name', 'department__name')
+    departments = Department.objects.all().values()
 
     context = {
         'dashboard_data': {
             'Functions': {
-                'value': 3,
-                'icon': 'function.svg'
+                'value': functions.count(),
+                'icon': 'function.svg',
+                'color': '#9ef01a12'
             },
             'Departments': {
-                'value': 5,
-                'icon': 'department.svg'
+                'value': departments.count(),
+                'icon': 'department.svg',
+                'color': '#3a86ff0d'
             },
             'Fails': {
                 'value': 2,
-                'icon': 'error.svg'
+                'icon': 'error.svg',
+                'color': '#ec00290d'
             }
-        }
+        },
+        'functions': functions,
+        'logs': logs
     }
 
     return HttpResponse(template.render(context, request))
+
+"""
+log_in
+------
+    This request is used to register the
+    entry of a Zoho function thats is monitored
+"""
+def log_in(request):
+    # Transform POST body data to dictionary
+    body_data = literal_eval(request.body.decode('utf-8'))
+
+    # Get POST data
+    function = body_data['function']
+    departmentId = body_data['department']
+    ticketId = body_data['ticket']
+
+    # Search on database for the required entities
+    # to create a new log
+    function_data = Function.objects.filter(name = function)
+    ticket_data = Ticket.objects.filter(zoho_id = ticketId)
+    department_data = Department.objects.filter(zoho_id = departmentId)
+
+    if ticket_data.count() == 0:
+        new_ticket = Ticket(zoho_id = ticketId)
+        new_ticket.save()
+        ticket_data = Ticket.objects.filter(zoho_id = ticketId)
+
+    if department_data.count() == 0:
+        new_department = Department(name = 'Test department', zoho_id = departmentId)
+        new_department.save()
+        department_data = Department.objects.filter(zoho_id = departmentId)
+
+    if function_data.count() == 0:
+        department_instance = Department(id = department_data.values()[0]['id'])
+        new_function = Function(name = function, department = department_instance)
+        new_function.save()
+        function_data = Function.objects.filter(name = function)
+
+    # Create a new function log
+    function_instance = Function(id = function_data.values()[0]['id'])
+    ticket_instamce = Ticket(id = ticket_data.values()[0]['id'])
+    new_log = Log(function = function_instance, ticket = ticket_instamce)
+    new_log.save()
+
+    return HttpResponse(1)
