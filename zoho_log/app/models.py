ac048811e6c27652140ae222e11c5293b26c1e29
Ramphy Aquino Nova <30904905+Ramphy0x90@users.noreply.github.com>
2022-10-31 15:01:58 +0100

    Add id to models

--- zoho_log/app/models.py
+++ zoho_log/app/models.py
@@ -2,17 +2,20 @@ from django.db import models
 
 """ Department Mmodel"""
 class Department(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     zoho_id = models.IntegerField(default=-1)
     country = models.CharField(max_length=255)
 
 """ Function Mmodel"""
 class Function(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
 
 """ Ticket Mmodel"""
 class Ticket(models.Model):
+    id = models.AutoField(primary_key=True)
     number = models.IntegerField(default=-1)
     zoho_id = models.IntegerField(default=-1)
     subject = models.CharField(max_length=255)
@@ -20,6 +23,7 @@ class Ticket(models.Model):
 
 """ Log Mmodel"""
 class Log(models.Model):
+    id = models.AutoField(primary_key=True)
     function = models.ForeignKey(Function, on_delete=models.CASCADE)
     ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
     log_in = models.DateTimeField(auto_now=True)
@@ -27,5 +31,6 @@ class Log(models.Model):
 
 """ Zoho token model """
 class ZohoToken(models.Model):
+    id = models.AutoField(primary_key=True)
     token = models.CharField(max_length=255)
     creation = models.DateTimeField(auto_now=True)
